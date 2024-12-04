f067e208c15b20d35fa1fb9c999da9d48afd45f1
danieloselu3 <doselu3@gmail.com>
2024-12-04 15:35:11 +0300

    added variables for cloud composer and the raw and cleanse layer buckets

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,22 @@
-# Storage Outputs
-output "raw_layer_bucket_name" {
-  description = "Name of the raw layer storage bucket"
-  value       = google_storage_bucket.raw_layer.name
+# GCS Data Layers Bucket Outputs
+output "data_layers_bucket_name" {
+  description = "Name of the data layers storage bucket"
+  value       = google_storage_bucket.data_layers.name
 }
 
-output "raw_layer_bucket_url" {
-  description = "URL of the raw layer storage bucket"
-  value       = google_storage_bucket.raw_layer.url
+output "data_layers_bucket_url" {
+  description = "URL of the data layers storage bucket"
+  value       = google_storage_bucket.data_layers.url
+}
+
+output "raw_layer_path" {
+  description = "Path to the raw data layer in the bucket"
+  value       = "gs://${google_storage_bucket.data_layers.name}/raw/"
+}
+
+output "cleanse_layer_path" {
+  description = "Path to the cleansed data layer in the bucket"
+  value       = "gs://${google_storage_bucket.data_layers.name}/cleanse/"
 }
 
 # BigQuery Outputs
@@ -40,4 +50,20 @@ output "data_network_name" {
 output "data_subnetwork_name" {
   description = "Name of the data subnetwork"
   value       = google_compute_subnetwork.data_subnetwork.name
+}
+
+# Cloud Composer Outputs
+output "composer_environment_name" {
+  description = "Name of the Cloud Composer environment"
+  value       = google_composer_environment.ecommerce_airflow.name
+}
+
+output "composer_environment_web_server_url" {
+  description = "Web server URL of the Cloud Composer environment"
+  value       = google_composer_environment.ecommerce_airflow.config[0].airflow_uri
+}
+
+output "composer_environment_gke_cluster" {
+  description = "GKE cluster used by the Cloud Composer environment"
+  value       = google_composer_environment.ecommerce_airflow.config[0].gke_cluster
 }
